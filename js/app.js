50b88f08c0d2ed7940c178371c9f627e87779049
Radostina-Dimitrova <radostinadimitrova@gmail.com>
2018-04-23 01:48:49 +0300

    Update the enemy's position

--- js/app.js
+++ js/app.js
@@ -18,7 +18,13 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-};
+    this.x += this.speed * dt; 
+
+    if(this.x >= 505) {
+       this.y = 63 + (Math.round(Math.random() * 2) * 83);
+       this.x = -50;
+   }
+}
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
